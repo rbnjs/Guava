045f41cb1d19d486b80f4b95e40fb005da572883
RubenSerradas <rubenserradas@gmail.com>
2014-03-05 14:21:16 -0430

    Segundo prototipo.

--- GuavaSymTable.hh
+++ GuavaSymTable.hh
@@ -15,15 +15,19 @@
  *
  * =====================================================================================
  */
+# include "Symbol.hh"
 class GuavaSymTable{
 public:
     GuavaSymTable(); /* Constructor de la clase */
     virtual ~GuavaSymTable(); /*  Destructor */
 
+    void enterScope();
+    void exitScope();
     void insert(/*  Simbolo a insertar  */);              /* Inserta un simbolo a la tabla */
-    void lookup(const std::string elem, int alcance);     /*  Busca un simbolo en la tabla y retorna NULL o el simbolo. */
+    Symbol lookup(const std::string elem, int alcance);     /*  Busca un simbolo en la tabla y retorna NULL o el simbolo. */
     void show();                                          /* Muestra la tabla */
 private:
     std::stack<int> pila;
     std::map<std::string, std::list<Symbol>> tabla;
+    int alcance;
 }

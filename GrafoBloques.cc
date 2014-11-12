ab13cfe3a182e81d879614b75db5c1155c69b45f
RubenSerradas <rubenserradas@gmail.com>
2014-11-11 22:13:55 -0430

    Realizado el grafo.

--- GrafoBloques.cc
+++ GrafoBloques.cc
@@ -19,6 +19,10 @@
 #include <unordered_map>
 #include <utility>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/depth_first_search.hpp>
+#include <boost/graph/two_bit_color_map.hpp>
+#include <boost/graph/graph_concepts.hpp>
+#include <boost/graph/graph_traits.hpp>
 
 using namespace std;
 int id_unica = 0 ; /* Id unica para BloqueBasico  */
@@ -48,14 +52,13 @@ BloqueBasico::BloqueBasico(){
  * Constructor para Entries y Exits.
  * @param o Opcion a elegir 0 es Entry y todo lo demas es Exit
  */
-BloqueBasico::BloqueBasico(int o): id(id_unica){
+BloqueBasico::BloqueBasico(int o, string* belongs): id(id_unica), belongs_to(belongs){
     if (o == 0){
         is_entry_ = true;
     }else {
         is_exit_ = true;
     }
     id_unica++; // Aumento el id_unico
-
 }
 
 /** 
@@ -78,6 +81,7 @@ void BloqueBasico::print(ostream &os){
     for (list<GuavaQuads*>::iterator it = codigo.begin(); it != codigo.end(); ++it){
         os << (*it)->gen() << endl;
     }
+    if (this->get_belongs_to() != 0) os << "Pertenece: " << *this->get_belongs_to() << endl; 
     os << "----" <<endl;
 }
 
@@ -137,10 +141,6 @@ list<BloqueBasico*> obtener_bloques(list<GuavaQuads*>* codigo, list<GuavaQuads*>
         if (!codigo_bloque.empty()){
             BloqueBasico* nuevo_bloque = new BloqueBasico(codigo_bloque);
             result.push_back(nuevo_bloque);
-
-            if (nuevo_bloque->first()->is_func_label()){
-                result.push_front(new BloqueBasico(0)); // Genero un bloque entry para cada funcion.
-            }
         }
         ++it_lideres;
     }
@@ -162,26 +162,26 @@ list<BloqueBasico*> obtener_bloques(list<GuavaQuads*>* codigo, list<GuavaQuads*>
  * @param bloques Todos los bloques del grafo.
  * @return result Lista de lados.
  */
-list<pair<BloqueBasico*,BloqueBasico*>> obtener_lados(list<BloqueBasico*> bloques){
+list<pair<BloqueBasico*,BloqueBasico*>> obtener_lados(list<BloqueBasico*>* bloques){
     list<pair<BloqueBasico*, BloqueBasico*>> result;
     BloqueBasico* bloque;
 
     //Primero voy a poner los lados de los bloques secuenciales. (a)
-    for (list<BloqueBasico*>::iterator it = bloques.begin() ; it != bloques.end(); ++it){
+    for (list<BloqueBasico*>::iterator it = bloques->begin() ; it != bloques->end(); ++it){
         bloque = *it;
         //Si tiene un salto incondicional al final entonces no colocar el lado.
-        if ( it != bloques.begin() && !(*std::prev(it))->is_entryexit() && !(*std::prev(it))->last()->is_jump()){
+        if ( it != bloques->begin() && !(*std::prev(it))->last()->is_jump() &&  !(*std::prev(it))->last()->is_return()){
             pair<BloqueBasico*,BloqueBasico*> par_tmp (*std::prev(it),bloque);
             result.push_back(par_tmp);
         }
     }
 
     // Agregando los lados resultados de saltos. (b)
-    for (list<BloqueBasico*>::iterator it = bloques.begin(); it != bloques.end(); ++it){
-        for (list<BloqueBasico*>::iterator it_2 = bloques.begin(); it_2 != bloques.end() ; ++it_2){
+    for (list<BloqueBasico*>::iterator it = bloques->begin(); it != bloques->end(); ++it){
+        for (list<BloqueBasico*>::iterator it_2 = bloques->begin(); it_2 != bloques->end() ; ++it_2){
             BloqueBasico* b = *it;
             BloqueBasico* c = *it_2;
-            if ( !b->is_entryexit() && !c->is_entryexit() && b->last()->is_goto()){
+            if (b->last()->is_goto()){
                 if ( b->last()->same_label(c->first()) ){
                     pair<BloqueBasico*,BloqueBasico*> par_tmp (b,c);
                     result.push_back(par_tmp);
@@ -189,9 +189,87 @@ list<pair<BloqueBasico*,BloqueBasico*>> obtener_lados(list<BloqueBasico*> bloque
             }
         }
     }
-    return result;
 
+    return result;
 }
+
+/**
+ * Agrega los exit a cada funcion junto con sus lados. 
+ * @param g Grafo
+ * @param dict Diccionario que contiene los bloques basicos junto con su descriptor de nodo.
+ */
+void agregar_exits(list<BloqueBasico*> bloques, std::unordered_map<BloqueBasico*, Graph::vertex_descriptor> dict, Graph& grafo){
+    std::unordered_map<string, Graph::vertex_descriptor> bloques_exit; /* De esta manera guardamos todos los bloques exit para que no se repitan. */
+
+    for (list<BloqueBasico*>::iterator it = bloques.begin();  it != bloques.end(); ++it){
+       if ( !(*it)->is_entryexit() && (*it)->last()->is_return()){
+            Graph::vertex_descriptor v;
+            if ( grafo[dict[*it]].get_belongs_to() != 0){
+                if (bloques_exit.count(*grafo[dict[*it]].get_belongs_to()) == 0){
+                    //Bloque no existe aun. Lo agrego en el grafo y en el diccionario.
+                    bloques_exit[*grafo[dict[*it]].get_belongs_to()] = add_vertex(grafo);
+                    v = bloques_exit[*grafo[dict[*it]].get_belongs_to()];
+                    BloqueBasico tmp(1,grafo[dict[*it]].get_belongs_to());
+                    grafo[v].clone(&tmp);
+                }else{
+                   v = bloques_exit[*grafo[dict[*it]].get_belongs_to()]; 
+                }
+                add_edge(dict[*it],v,grafo);
+            }else {
+                remove_vertex(dict[*it],grafo); // Elimino nodo al que no se puede llegar.
+            }
+       }
+    }
+}
+
+/** 
+ * Agrega los bloque entry a cada funcion junto con sus lados.
+ * @param g Grafo
+ * @param dict Diccionario que contiene los bloques basicos junto con su descriptor de nodo.
+ * @returns Lista de nodos entry.
+ */
+list<Graph::vertex_descriptor> agregar_entry(list<BloqueBasico*> bloques,std::unordered_map<BloqueBasico*, Graph::vertex_descriptor> dict, Graph& grafo){
+    list<Graph::vertex_descriptor> result;
+    for (list<BloqueBasico*>::iterator it = bloques.begin();  it != bloques.end(); ++it){
+       if ((*it)->first()->is_func_label()){
+            BloqueBasico entry(0,new string((*it)->first()->get_op()));
+            Graph::vertex_descriptor v = add_vertex(grafo);
+            result.push_back(v);
+            grafo[v].clone(&entry);
+            add_edge(v, dict[*it],grafo);
+       }
+    }
+    return result;
+}
+
+/** 
+ * Hace algo asi como un dfs (creo) identificando cada uno de los nodos observados.
+ * @root Raiz.
+ */
+void identificador_recursivo(Vertex root, Graph& grafo){
+    typename graph_traits<Graph>::out_edge_iterator out_i, out_end;  
+    typename graph_traits<Graph>::edge_descriptor e;
+    for (boost::tie(out_i, out_end) = out_edges(root, grafo);
+                out_i != out_end; ++out_i){
+        e = *out_i;        
+        Vertex targ = target(e, grafo);
+        if (grafo[targ].get_belongs_to() == 0) grafo[targ].set_belonging(grafo[root].get_belongs_to());
+        identificador_recursivo(targ,grafo);
+    }
+}
+
+/** 
+ * Identifica cada bloque segun la función a la que pertenece. 
+ * Por ejemplo a cada bloque asociado al bloque entry main le asocio el nombre main.
+ */
+void identificar_bloques(list<Vertex> entries, Graph& grafo){
+
+    for (list<Vertex>::iterator entry = entries.begin(); entry != entries.end(); ++entry){
+        identificador_recursivo(*entry,grafo);
+    }
+}
+
+
 
 /**  
  * Constructor de GrafoFlujo.
@@ -213,13 +291,16 @@ GrafoFlujo::GrafoFlujo(list<GuavaQuads*>* codigo){
         grafo[v].clone(tmp);
     }
 
-    list<pair<BloqueBasico*,BloqueBasico*> > lados = obtener_lados(bloques);
+    list<pair<BloqueBasico*,BloqueBasico*> > lados = obtener_lados(&bloques);
 
     //Agregando los lados.
     for (list<pair<BloqueBasico*, BloqueBasico*> >::iterator it = lados.begin(); it != lados.end(); ++it){
         pair<BloqueBasico*, BloqueBasico*> tmp = *it;
         add_edge(dict[tmp.first],dict[tmp.second],grafo);
     }
+    entries = agregar_entry(bloques,dict,grafo);
+    identificar_bloques(entries,grafo);
+    agregar_exits(bloques,dict,grafo);
 
     this->imprimir_nodos(cout);
     this->imprimir_lados(cout);
@@ -237,11 +318,13 @@ void GrafoFlujo::imprimir_nodos(ostream& os){
 
 }
 
+/** 
+ * Imprime los lados.
+ */
 void GrafoFlujo::imprimir_lados(ostream& os){
     graph_traits<Graph>::vertex_iterator vi, vi_end, next;
     typename graph_traits<Graph>::out_edge_iterator out_i, out_end;  
     typename graph_traits<Graph>::edge_descriptor e;
-    typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
     boost::tie(vi, vi_end) = vertices(grafo);
     for (next = vi; next != vi_end; ++next){
         os << "Lados del Bloque :" << grafo[*next].get_id() << endl;
